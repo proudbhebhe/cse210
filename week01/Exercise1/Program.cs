d1a8b11ab5ddcc283fe74a29a59c0c5d967b77f0
proudbhebhe <proudbhebhe02@gmail.com>
2026-07-03 22:36:08 +0200

    made changes to exercise one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your surname? ");
+        string surname =Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your final score? ");
+        string score = Console.ReadLine();
+        int mark = int.Parse(score);
+
+        if (90<=mark)
+        {
+            Console.WriteLine("A");
+        }
+        else if (80<=mark)
+        {
+            Console.WriteLine("B");
+        }
+        else if (70<=mark)
+        {
+            Console.WriteLine("C");
+
+        }
     }
 }
